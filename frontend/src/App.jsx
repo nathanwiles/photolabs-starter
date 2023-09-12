1ee4600deeb1880cbf81a73a0646dacb9c057f82
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-09-12 00:51:29 -0400

    rework to use sample dataset for PhotoList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,77 @@
 import React from "react";
 
-import PhotoListItem from "./components/PhotoListItem";
+import PhotoList from "./components/PhotoList";
 import "./App.scss";
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
+const sampleDataForPhotoList = [
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    },
+    user: {
+      id: "1",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
   },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+  {
+    id: "2",
+    location: {
+      city: "Toronto",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
+    },
+    user: {
+      id: "2",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+  },
+  {
+    id: "3",
+    location: {
+      city: "Ottawa",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
+    },
+    user: {
+      id: "3",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+  },
+];
 
-const SamplePhotoList = (photoData) => {
-  const props = {photoData};
-  const photos = [];
-  for (let i = 0; i < 3; i++) {
-    photos[i] = PhotoListItem(props);
-  }
-  return photos;
-};
+// const SamplePhotoList = (photoData) => {
+//   const props = {photoData};
+//   const photos = [];
+//   for (let i = 0; i < 3; i++) {
+//     photos[i] = PhotoListItem(props);
+//   }
+//   return photos;
+// };
 
-const photos = SamplePhotoList(sampleDataForPhotoListItem);
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  return <div className="App">{photos}</div>;
+  return <div className="App">
+    <PhotoList photoDataObjs={sampleDataForPhotoList}/>
+  </div>;
 };
 
 export default App;
