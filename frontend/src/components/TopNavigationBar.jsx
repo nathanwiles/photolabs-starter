c915ac1987a75304976b9d137b8568df680c4c1c
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-09-17 16:55:22 -0400

    Create nav bar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import '../styles/TopNavigationBar.scss'
+import "../styles/TopNavigationBar.scss";
+import TopicList from "./TopicList";
+import FavIcon from "./FavIcon";
 
-const TopNavigation = () => {
+
+const TopNavigation = (props) => {
+  const {topicData} = props;
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList topicList={topicData} />
+      <FavIcon selected={true} />
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;
