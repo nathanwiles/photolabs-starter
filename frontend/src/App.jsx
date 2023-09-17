05f0921f6caeae123d024e6fb575dcb3e1d4e947
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-09-17 17:19:37 -0400

    setup app to use HomeRoute,js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,77 +1,19 @@
+import "./App.scss";
+import "styles/HomeRoute.scss";
 import React from "react";
 
-import PhotoList from "./components/PhotoList";
-import "./App.scss";
-
-const sampleDataForPhotoList = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    },
-    user: {
-      id: "1",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "2",
-    location: {
-      city: "Toronto",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
-    },
-    user: {
-      id: "2",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "3",
-    location: {
-      city: "Ottawa",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
-    },
-    user: {
-      id: "3",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-];
-
-// const SamplePhotoList = (photoData) => {
-//   const props = {photoData};
-//   const photos = [];
-//   for (let i = 0; i < 3; i++) {
-//     photos[i] = PhotoListItem(props);
-//   }
-//   return photos;
-// };
-
+import photos from "./mocks/photos";
+import topics from "mocks/topics";
+// Components
+import HomeRoute from "routes/HomeRoute";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  return <div className="App">
-    <PhotoList photoDataObjs={sampleDataForPhotoList}/>
-  </div>;
+  return (
+    <div className="App">
+      <HomeRoute topicDataList={topics} photoDataList={photos} />
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -4,8 +4,6 @@ import '../styles/HomeRoute.scss';
 
 import TopNavigation from "../components/TopNavigationBar";
 import PhotoList from "../components/PhotoList";
-import photos from 'mocks/photos';
-import topics from 'mocks/topics';
 
 const HomeRoute = (props) => {
   const {topicDataList, photoDataList} = props;
