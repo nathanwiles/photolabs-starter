1461b2fb00a3571fee8396fae787f1670e973ead
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-09-17 17:18:09 -0400

    tidy

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -4,7 +4,9 @@ import "../styles/TopicList.scss";
 
 const TopicList = (props) => {
   const { topicDataList } = props;
-  const renderedTopics = topicDataList.map((topicData) => TopicListItem(topicData));
+  const renderedTopics = topicDataList.map((topicData) =>
+    TopicListItem(topicData)
+  );
 
   return <div className="top-nav-bar__topic-list">{renderedTopics}</div>;
 };
