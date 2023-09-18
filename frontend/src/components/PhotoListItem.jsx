03e462c351049e0221ca5cad70fbeffbb85f166b
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-09-18 00:24:54 -0400

    add simple click handler

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,13 +4,13 @@ import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoFavButton.scss";
 import "../styles/PhotoListItem.scss";
 
-import { Context } from "GlobalState";
+import { favContext } from "GlobalFavState";
 
 
 const PhotoListItem = (props) => {
   const [isFav, setIsFav] = useState(props.favDefault);
 
-  const [favs] = useContext(Context);
+  const [favs] = useContext(favContext);
   
   useEffect(() => {
     const fav = favs.includes(props.id) ? true : false;
@@ -18,10 +18,14 @@ const PhotoListItem = (props) => {
   },[favs]);
 
 
+  const handleClick = (e) => {
+    console.log(e.target.closest(".photo-list__item"));
+  };
+
   return (
     <li id={props.id} className="photo-list__item">
       <PhotoFavButton favDefault={props.favDefault} isFav={isFav} />
-      <img src={props.urls.regular} className="photo-list__image"></img>
+      <img onClick={handleClick} src={props.urls.regular} className="photo-list__image"></img>
       <div className="photo-list__user-details">
         <img src={props.profile} className="photo-list__user-profile"></img>
         <div className="photo-list__user-info">
