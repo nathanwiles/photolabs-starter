0fcf8c3412d45427df003b8c0a3b22c564b556d7
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-09-18 14:34:20 -0400

    setup modal state and props to be passed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.scss";
 import "styles/HomeRoute.scss";
-import { React, useState, createContext } from "react";
+import { React, useState, useContext } from "react";
 
 import GlobalFavState from "GlobalFavState";
 import photos from "./mocks/photos";
@@ -8,14 +8,34 @@ import topics from "mocks/topics";
 
 // Components
 import HomeRoute from "routes/HomeRoute";
-
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  //modal logic
+  const [modalDisplay, setModalDisplay] = useState(false);
+  const [modalPhoto, setModalPhoto] = useState({});
+
+  const openModal = (photoId) => {
+    const photoIndex = photos.findIndex((data) => data.id === photoId);
+    const photo = photos[photoIndex];
+    setModalPhoto(photo);
+    setModalDisplay(true);
+  };
+
+  const closeModal = () => {
+    setModalDisplay(false);
+    setModalPhoto({});
+  };
   return (
     <GlobalFavState>
       <div className="App">
-        <HomeRoute topicDataList={topics} photoDataList={photos} />
+        <HomeRoute
+          topicDataList={topics}
+          photoDataList={photos}
+          modal={{ openModal, closeModal }}
+        />
+        {modalDisplay && <PhotoDetailsModal modal={{ modalPhoto, openModal, closeModal }} />}
       </div>
     </GlobalFavState>
   );
