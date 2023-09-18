d5deb361650726c4d2549c1b13e06a09110307d0
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-09-18 14:35:46 -0400

    setup modal.

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import '../styles/PhotoDetailsModal.scss'
-import closeSymbol from '../assets/closeSymbol.svg';
+import "../styles/PhotoDetailsModal.scss";
+import closeSymbol from "../assets/closeSymbol.svg";
+import PhotoListItem from "components/PhotoListItem";
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = (props) => {
+  const photoData = props.modal.modalPhoto;
+
+  const handleClose = () => {
+    props.modal.closeModal();
+  };
+
   return (
-    <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
+    <div className="photo-details-modal__overlay">
+      <div className="photo-details-modal">
+        <button
+          onClick={handleClose}
+          className="photo-details-modal__close-button"
+        >
+          <img src={closeSymbol} alt="close symbol" />
+        </button>
+        <PhotoListItem
+          key={photoData.id}
+          {...photoData}
+          favDefault={false}
+          modal={props.modal}
+          imageClass="photo-details-modal__image"
+        />
+      </div>
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
