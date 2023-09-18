b7ac2b2cee743820bbc0706dfc683aa3f988a50f
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-09-18 14:29:24 -0400

    update to handle photo user data properly.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect, useContext} from "react";
+import { React, useState, useEffect, useContext } from "react";
 
 import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoFavButton.scss";
@@ -6,30 +6,35 @@ import "../styles/PhotoListItem.scss";
 
 import { favContext } from "GlobalFavState";
 
-
 const PhotoListItem = (props) => {
-  const [isFav, setIsFav] = useState(props.favDefault);
+  const [isFav, setIsFav] = useState(props.favDefault || false);
 
   const [favs] = useContext(favContext);
-  
+
   useEffect(() => {
     const fav = favs.includes(props.id) ? true : false;
     setIsFav(fav);
-  },[favs]);
-
+  }, [favs]);
 
   const handleClick = (e) => {
-    console.log(e.target.closest(".photo-list__item"));
+    const photoId = e.target.closest(".photo-list__item").id;
+
+    props.modal.openModal(photoId);
+   
   };
 
   return (
     <li id={props.id} className="photo-list__item">
       <PhotoFavButton favDefault={props.favDefault} isFav={isFav} />
-      <img onClick={handleClick} src={props.urls.regular} className="photo-list__image"></img>
+      <img
+        onClick={handleClick}
+        src={props.urls.regular}
+        className={props.imageClass ? props.imageClass : "photo-list__image"}
+      ></img>
       <div className="photo-list__user-details">
-        <img src={props.profile} className="photo-list__user-profile"></img>
+        <img src={props.user.profile} className="photo-list__user-profile"></img>
         <div className="photo-list__user-info">
-          {props.username}
+          {props.user.name}
           <div className="photo-list__user-location">
             {props.location.city}, {props.location.country},
           </div>
