2cce7fd4a3b42358fb0bf4596fdc4e2dc009edb5
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-09-24 12:47:06 -0400

    WIP setup to fetch data from backend. Modal is broken.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import "./App.scss";
 import "styles/HomeRoute.scss";
 import { React, useEffect } from "react";
-import photos from "./mocks/photos";
-import topics from "./mocks/topics";
 import useApplicationData from "hooks/useApplicationData";
 import { ACTIONS } from "hooks/useApplicationData";
+
 // Components
 import HomeRoute from "routes/HomeRoute";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
@@ -13,10 +12,6 @@ import PhotoDetailsModal from "routes/PhotoDetailsModal";
 const App = () => {
   const { dispatch, state } = useApplicationData();
 
-  useEffect(() => {
-    dispatch({type: ACTIONS.SET_PHOTO_DATA, payload: photos});
-    dispatch({type: ACTIONS.SET_TOPICS_DATA, payload: topics});
-  }, []);
 
   const { modalDisplay } = state;
 
--- frontend/src/hooks/useApplicationData.jsx
+++ frontend/src/hooks/useApplicationData.jsx
@@ -1,18 +1,18 @@
-import { React, useReducer} from "react";
-
+import { React, useReducer, useEffect } from "react";
 
 export const ACTIONS = {
   OPEN_MODAL: "OPEN_MODAL",
   CLOSE_MODAL: "CLOSE_MODAL",
   TOGGLE_FAV_BY_ID: "TOGGLE_FAV_BY_ID",
   SET_PHOTO_DATA: "SET_PHOTO_DATA",
-  SET_TOPICS_DATA: "SET_TOPICS_DATA"
+  SET_TOPICS_DATA: "SET_TOPICS_DATA",
 };
 
+// prettier-ignore
 const reducer = (state, { type, payload }) => {
   switch (type) {
   case "OPEN_MODAL": {
-    const {photos} = state;
+    const { photos } = state;
     const photoIndex = photos.findIndex((data) => data.id === payload);
     const modalPhoto = photos[photoIndex];
     return {
@@ -40,22 +40,23 @@ const reducer = (state, { type, payload }) => {
       newFavs.splice(index, 1);
       return {
         ...state,
-        favs: [...newFavs]
+        favs: [...newFavs],
       };
     }
   }
-  case "SET_PHOTO_DATA":
+  case "SET_PHOTO_DATA":{
+    
     return {
       ...state,
-      photos: payload
+      photos: payload,
     };
-  
+  }
   case "SET_TOPICS_DATA":
     return {
       ...state,
-      topics: payload
+      topics: payload,
     };
-  
+
   default:
     throw new Error(`Tried to reduce with unsupported action type: ${type}`);
   }
@@ -69,6 +70,20 @@ const useApplicationData = () => {
     photos: [],
     topics: [],
   };
+
+  useEffect(() => {
+    fetch("/api/photos")
+      .then((response) => response.json())
+      .then((data) =>
+        dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: data })
+      );
+
+    fetch("/api/topics")
+      .then((response) => response.json())
+      .then((data) =>
+        dispatch({ type: ACTIONS.SET_TOPICS_DATA, payload: data })
+      );
+  }, []);
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
