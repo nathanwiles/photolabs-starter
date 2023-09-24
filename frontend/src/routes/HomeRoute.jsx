20dcfc7aa152d6cb206a3f30fa585fce4cbc700e
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-09-24 14:29:46 -0400

    change styling back to small cards on home page

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -5,24 +5,21 @@ import "../styles/HomeRoute.scss";
 import TopNavigation from "../components/TopNavigationBar";
 import PhotoList from "../components/PhotoList";
 
-
 const HomeRoute = (props) => {
-  const {state} = props;
- 
+  const { state } = props;
 
   return (
     <div className="home-route">
-      <TopNavigation state={state}/>
+      <TopNavigation state={state} />
       <PhotoList
         {...props}
         photoDataList={state.photos}
         favDefault={false}
-        favIconSize="large"
-        listClassName="photo-list__vertical"
+        favIconSize="small"
+        listClassName="photo-list"
         photoClassName="photo-list__image"
         userDetailsClassName="photo-list__user-details"
       />
-      
     </div>
   );
 };
